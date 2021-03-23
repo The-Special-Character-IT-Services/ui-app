19b909c74c88734c7878e248203a31e7af7cc54a
rohitkumar123 <rkmishra0484@gmail.com>
2021-03-23 21:11:21 +0530

    commit

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -10,9 +10,9 @@ class Filter extends Component {
     return (
       <div style={{ flex: 1, padding: 4,flexDirection:'row' }}>
             
-          <img src={FilterLogo} alt="" onClick={() => this.setState({show:!show })} style={{ height: 30, width: 30 }}></img>
-          {show && <span style={{backgroundColor:'red',padding:8,marginBottom:8}}>
-           Heelo
+          <img src={FilterLogo} alt="" onClick={() => this.setState({show:!show })} style={{ height: 30, width: 30}}></img>
+          {show && <span style={{padding:8,marginBottom:8,position:'absolute',marginLeft:8}}>
+           Close filters
             </span>}
 
         {show && <div style={{paddingTop:15}}>
